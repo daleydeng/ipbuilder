fb21746c624c5fdb22d5999ec1f2ef451841cae6
daleydeng <daleydeng@qingtong123.com>
2021-02-15 08:36:34 +0800

    add check length in uploader

--- tools/upload_bilibili.py
+++ tools/upload_bilibili.py
@@ -12,7 +12,12 @@ import click
 def file_name(f):
     return osp.basename(osp.splitext(f)[0])
 
+def check_length(name):
+    assert len(name) < 64, f"name is too long: {name}"
+
 def main(src_d: str, title: str, cfg_f='~/.config/bilibili.toml', copyright=2, tid=208, tag='', desc='', source='course', jobs=1):
+
+    check_length(title)
 
     cfg_f = osp.expanduser(cfg_f)
     assert osp.exists(cfg_f)
@@ -29,6 +34,8 @@ def main(src_d: str, title: str, cfg_f='~/.config/bilibili.toml', copyright=2, t
     parts = []
     for video_f in sorted(glob(src_d+'/*.mp4')):
         name = file_name(video_f)
+
+        check_length(name)
         vid = VideoPart(
             path=video_f,
             title=name,
